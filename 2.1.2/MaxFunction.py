4a6d25a91e804e6bff7ef5e9cc2e4be991a69edb
gabriellima <gabrielkvalknt@gmail.com>
2017-10-06 18:14:49 -0300

    correção no max function

--- 2.1.2/MaxFunction.py
+++ 2.1.2/MaxFunction.py
@@ -1,5 +1,7 @@
 from random import uniform, random
 import math
+import time
+
 
 def anneal(x,y):
     old_cost = function(x,y)
@@ -20,32 +22,33 @@ def anneal(x,y):
                 y=y_new
                 old_cost = new_cost
             else:  
-            	ap = acceptance_probability(old_cost, new_cost, T)
-            	rand = random()
-            	if (ap > rand and delta_e<-0.00000001):
-            		new_cost = function(x_new, y_new)
-            		old_cost = new_cost
-            		x = x_new
-            		y = y_new
+                ap = acceptance_probability(old_cost, new_cost, T)
+                rand = random()
+                if (ap > rand and delta_e<-0.00000001):
+                    new_cost = function(x_new, y_new)
+                    old_cost = new_cost
+                    x = x_new
+                    y = y_new
             i += 1
             if old_cost>maximo:
-            	maximo = old_cost
-            	x_max = x
-            	y_max = y
+                maximo = old_cost
+                x_max = x
+                y_max = y
         T = T*alpha
     return x, y, old_cost
 
 def function(x,y):
     return (4*(math.exp(-(x**2+y**2)))+
-    			math.exp(-((x-5)**2+(y-5)**2))+
-    			math.exp(-((x+5)**2+(y-5)**2))+
-    			math.exp(-((x-5)**2+(y+5)**2))+
-    			math.exp(-((x+5)**2+(y+5)**2)))
+                math.exp(-((x-5)**2+(y-5)**2))+
+                math.exp(-((x+5)**2+(y-5)**2))+
+                math.exp(-((x-5)**2+(y+5)**2))+
+                math.exp(-((x+5)**2+(y+5)**2)))
 
 def acceptance_probability(old_cost, new_cost, T):
-	return math.exp((new_cost-old_cost)/T)
+    return math.exp((new_cost-old_cost)/T)
 
-x=uniform(-1,1)*50
-y=uniform(-1,1)*50
-print(x,y)
-print(anneal(x,y))
+x=uniform(-1,1)*20
+y=uniform(-1,1)*20
+print("Resultado: ")
+print(anneal(x,y)[2])
+time.sleep(5)
